53b810580cb55fc85c4ad99b8761cc986392ad43
kotenochek27 <katrinya1@gmail.com>
2023-07-30 15:47:53 -0400

    add files

---
+++ script.js
@@ -0,0 +1,30 @@
+function fetchData() {
+    const baseUrl = 'https://swapi.dev/api';
+    const entityType = 'people';
+    const entityId = Math.floor(Math.random() * 10) + 1; // Генерируем случайный числовой идентификатор от 1 до 10
+    const url = `${baseUrl}/${entityType}/${entityId}`;
+
+    const loadingMessage = document.getElementById('loading');
+    const resultContainer = document.getElementById('result');
+    const errorContainer = document.getElementById('error');
+
+    loadingMessage.style.display = 'block';
+    resultContainer.textContent = '';
+    errorContainer.textContent = '';
+
+    fetch(url)
+        .then(response => {
+            if (!response.ok) {
+                return Promise.reject(`Ошибка: ${response.status}`);
+            }
+            return response.json();
+        })
+        .then(data => {
+            loadingMessage.style.display = 'none';
+            resultContainer.textContent = `Имя: ${data.name}, Рост: ${data.height}, Вес: ${data.mass}`;
+        })
+        .catch(error => {
+            loadingMessage.style.display = 'none';
+            errorContainer.textContent = `Ошибка: ${error}. Сервер не доступен.`;
+        });
+}
